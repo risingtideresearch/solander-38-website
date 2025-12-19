5faf3cb4718ec06556a79afa770c6ff4a0d15bac
Shelby Wilson <s.wilson024@gmail.com>
2025-12-19 11:41:25 -0800

    styling

--- frontend/app/people/page.tsx
+++ frontend/app/people/page.tsx
@@ -36,120 +36,108 @@ export default async function Page() {
               .map((person) => {
                 return (
                   <div key={person._id} className={styles.person}>
-                    <div>
-                      <p
-                        style={{
-                          margin: 0,
-                          fontWeight: 600,
-                        }}
-                      >
-                        {person.name}
-                      </p>
-                    </div>
+                    <p
+                      style={{
+                        fontWeight: 600,
+                      }}
+                    >
+                      {person.name}
+                    </p>
                     {person.role ? (
-                      <div
-                        style={{
-                          margin: "1rem 0 0 0",
-                        }}
-                      >
+                      <div>
                         <h6>Role</h6>
-                        <p style={{ margin: "0 0 0 3rem" }}>{person.role}</p>
+                        <p>{person.role}</p>
                       </div>
                     ) : (
                       <></>
                     )}
                     {person.link ? (
-                      <div
-                        style={{
-                          margin: "1rem 0 0 0",
-                        }}
-                      >
+                      <div>
                         <h6>Website</h6>
-                        <p style={{ margin: "0 0 0 3rem" }}>
+                        <p>
                           <a href={person.link.url} target="_blank">
-                            {person.link.label || person.link.url.replace('https://', '').replace('.com/', '.com')}
+                            {person.link.label ||
+                              person.link.url
+                                .replace("https://", "")
+                                .replace(".com/", ".com")}
                           </a>
                         </p>
                       </div>
                     ) : (
                       <></>
                     )}
-                    <div>
-                      {person.articlesAsAuthor.length > 0 ? (
+                    {person.articlesAsAuthor.length > 0 ? (
+                      <div>
+                        <h6>Author</h6>
                         <div>
-                          <h6>Author</h6>
-                          <p>
-                            {person.articlesAsAuthor
-                              .sort((a, b) =>
-                                getArticleId(a.slug).localeCompare(
-                                  getArticleId(b.slug),
-                                ),
-                              )
-                              .map((article, i, all) => (
-                                <div style={{ margin: 0 }} key={article._id}>
-                                  <a
+                          {person.articlesAsAuthor
+                            .sort((a, b) =>
+                              getArticleId(a.slug).localeCompare(
+                                getArticleId(b.slug),
+                              ),
+                            )
+                            .map((article, i, all) => (
+                              <p style={{ margin: 0 }} key={article._id}>
+                                <a
+                                  style={{
+                                    display: "grid",
+                                    gridTemplateColumns: "2rem 1fr",
+                                  }}
+                                  href={`/stories/${article.slug}`}
+                                >
+                                  <span
                                     style={{
-                                      display: "grid",
-                                      gridTemplateColumns: "2.5rem 1fr",
-                                      marginLeft: "3rem",
+                                      fontSize: "0.75rem",
+                                      textTransform: "uppercase",
                                     }}
-                                    href={`/stories/${article.slug}`}
                                   >
-                                    <span
-                                      style={{
-                                        fontSize: "0.75rem",
-                                        textTransform: "uppercase",
-                                      }}
-                                    >
-                                      {getArticleId(article.slug)}
-                                    </span>{" "}
-                                    {article.title}
-                                  </a>
-                                </div>
-                              ))}
-                          </p>
+                                    {getArticleId(article.slug)}
+                                  </span>{" "}
+                                  {article.title}
+                                </a>
+                              </p>
+                            ))}
                         </div>
-                      ) : (
-                        <div></div>
-                      )}
-                      {person.articlesMentioned.length > 0 ? (
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                    {person.articlesMentioned.length > 0 ? (
+                      <div>
+                        <h6>Part of</h6>
                         <div>
-                          <h6>Part of</h6>
-                          <p>
-                            {person.articlesMentioned
-                              .sort((a, b) =>
-                                getArticleId(a.slug).localeCompare(
-                                  getArticleId(b.slug),
-                                ),
-                              )
-                              .map((article, i, all) => (
-                                <div style={{ margin: 0 }} key={article._id}>
-                                  <a
+                          {person.articlesMentioned
+                            .sort((a, b) =>
+                              getArticleId(a.slug).localeCompare(
+                                getArticleId(b.slug),
+                              ),
+                            )
+                            .map((article, i, all) => (
+                              <p style={{ margin: 0 }} key={article._id}>
+                                <a
+                                  style={{
+                                    display: "grid",
+                                    gridTemplateColumns: "2rem 1fr",
+                                  }}
+                                  href={`/stories/${article.slug}`}
+                                >
+                                  <span
                                     style={{
-                                      display: "grid",
-                                      gridTemplateColumns: "2.5rem 1fr",
-                                      marginLeft: "3rem",
+                                      fontSize: "0.75rem",
+                                      textTransform: "uppercase",
                                     }}
-                                    href={`/stories/${article.slug}`}
                                   >
-                                    <span
-                                      style={{
-                                        fontSize: "0.75rem",
-                                        textTransform: "uppercase",
-                                      }}
-                                    >
-                                      {getArticleId(article.slug)}
-                                    </span>{" "}
-                                    {article.title}
-                                  </a>
-                                </div>
-                              ))}
-                          </p>
+                                    {getArticleId(article.slug)}
+                                  </span>{" "}
+                                  {article.title}
+                                </a>
+                              </p>
+                            ))}
                         </div>
-                      ) : (
-                        <div></div>
-                      )}
-                    </div>
+                      </div>
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 );
               })}
