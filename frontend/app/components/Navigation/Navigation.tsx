3e0ddf2496fcd910018e59024dc6c3093f1115cd
Shelby Wilson <s.wilson024@gmail.com>
2026-01-08 14:52:44 -0800

    adjust nav

--- frontend/app/components/Navigation/Navigation.tsx
+++ frontend/app/components/Navigation/Navigation.tsx
@@ -68,15 +68,43 @@ export default function Navigation({
 
   const links = (
     <>
-      {nav.map((link) => (
-        <a
-          key={link.label}
-          href={getURL(link)}
-        >
-          <span style={{ fontWeight: 600, visibility: "hidden" }}>
+      {nav.map((link, i) => (
+        <a key={link.label} href={getURL(link)} style={i == 0 ? {display: 'grid', gap: '0.625rem', gridTemplateColumns: '0.5rem 1fr'} : {}}>
+          {i == 0 ? (
+            <svg
+              className={styles.logo}
+              width="59"
+              height="78"
+              viewBox="0 0 59 78"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M16.9 77.6C20 68.8 24.8 54.9 26.7 49.5H0L7.7 40.7C18.5 37.7 23.4 39.9 28.3 42C32.8 44 37.2 45.9 44.5 45.3L16.9 77.6Z"
+                fill="black"
+              />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M42.4 0C39.3 8.8 34.5 22.7 32.6 28.1H58.9L51.7 36.8C40.8 39.9 36 37.8 31.1 35.7C26.7 33.8 22.2 31.8 15 32.4L42.4 0Z"
+                fill="black"
+              />
+            </svg>
+          ) : (
+            <></>
+          )}
+          <span style={{ fontWeight: 600 }}>
             {link.label}
           </span>
-          <span style={{ position: "absolute", left: 0, fontWeight: active == link.url ? 600 : "" }}>{link.label}</span>
+          <span
+            style={{
+              fontWeight: active == link.url ? 600 : "",
+            }}
+          >
+            {link.label}
+          </span>
         </a>
       ))}
     </>
@@ -84,34 +112,10 @@ export default function Navigation({
   return (
     <>
       <nav className={`${styles.nav} pane ${type ? styles[type] : ""}`}>
-        <span>
-          <svg
-            className={styles.logo}
-            width="59"
-            height="78"
-            viewBox="0 0 59 78"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M16.9 77.6C20 68.8 24.8 54.9 26.7 49.5H0L7.7 40.7C18.5 37.7 23.4 39.9 28.3 42C32.8 44 37.2 45.9 44.5 45.3L16.9 77.6Z"
-              fill="black"
-            />
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M42.4 0C39.3 8.8 34.5 22.7 32.6 28.1H58.9L51.7 36.8C40.8 39.9 36 37.8 31.1 35.7C26.7 33.8 22.2 31.8 15 32.4L42.4 0Z"
-              fill="black"
-            />
-          </svg>
-
-          {links}
-        </span>
+        <span>{links}</span>
       </nav>
 
-      <MobileNavigation active={nav.find(link => link.url == active)?.label}>
+      <MobileNavigation active={nav.find((link) => link.url == active)?.label}>
         {links}
       </MobileNavigation>
     </>
