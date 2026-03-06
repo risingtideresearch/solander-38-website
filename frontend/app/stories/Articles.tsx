47e36b40fb72670b796a7f09c1a6608cef9cb45e
Shelby Wilson <s.wilson024@gmail.com>
2026-03-06 14:33:14 -0800

    adding preview images to table of contents

--- frontend/app/stories/Articles.tsx
+++ frontend/app/stories/Articles.tsx
@@ -1,17 +1,16 @@
 import styles from "./articles.module.scss";
 import { fetchSystems } from "@/sanity/lib/utils";
 import { formatDate } from "../utils";
+import RangeChart from "../components/RangeChart";
+import Image from "next/image";
 
 export default async function Articles({ subtitles }) {
   const { data } = await fetchSystems();
 
   return (
     <div className={styles.articles}>
-      <h1
-      >
-        Table of contents
-      </h1>
-      <main >
+      <h1>Table of contents</h1>
+      <main>
         {(data.systems || []).map((system, i) => (
           <section className="section--two-col" key={system._key}>
             <div></div>
@@ -23,6 +22,18 @@ export default async function Articles({ subtitles }) {
               <ol>
                 {(system.articles || []).map((article) => (
                   <li key={article._id}>
+                    <div className={styles["article-preview"]}>
+                      {article.slug == "range" ? (
+                        <RangeChart title={''} />
+                      ) : (
+                        <Image
+                          src={`/preview/${article.slug}.png`}
+                          width={1600}
+                          height={840}
+                          alt={`Model of ${article.title}`}
+                        />
+                      )}
+                    </div>
                     <div className={styles["article-header"]}>
                       <h6>{article.articleId}</h6>
                       <a
@@ -31,13 +42,14 @@ export default async function Articles({ subtitles }) {
                         style={{ fontSize: "0.875rem" }}
                       >
                         <p>{article.title}</p>
-                        {/* <LiaLongArrowAltRightSolid size={18} /> */}
                         <div></div>
                         <h6>
                           {article.isLive ? (
                             formatDate(article._updatedAt)
                           ) : (
-                            <em>in progress</em>
+                            <em style={{ color: "var(--muted)" }}>
+                              in progress
+                            </em>
                           )}
                         </h6>
                       </a>
