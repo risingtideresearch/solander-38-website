6cf6a79482c427239a1b6ed1a7153699617cc142
Shelby Wilson <s.wilson024@gmail.com>
2026-05-15 11:03:17 -0500

    fix build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  turbopack: {
+    root: __dirname,
+  },
   /* TODO remove */
   typescript: {
     ignoreBuildErrors: true,
