41e9f7c10a2bcacc707cc47ef6c1097f6a3cfdc9
Shelby Wilson <s.wilson024@gmail.com>
2026-05-27 13:26:15 -0700

    remove unused schemas

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,29 +1,15 @@
-// import {anatomy} from './anatomy'
-// import { anatomy } from './anatomy'
 import {article} from './article'
 import {imageAsset} from './imageAsset'
-// import {boat} from './boat'
 import {component} from './component'
 import { homepage } from './homepage'
-// import {connection} from './connection'
-// import {customPart} from './customPart'
 import {location} from './location'
 import { materials, material } from './materials'
-// import {model3d} from './model3d'
 import {person} from './person'
-// import {powerBudget} from './powerBudget'
-// import {schematic} from './schematic'
-// import {timeline} from './timeline'
-// import {annotation} from './annotation'
-// import { section, sections } from './sections'
 import { system, systems } from './systems'
 
 export const schemaTypes = [
   article,
-  // boat,
-  // anatomy,
   location,
-  // customPart,
   component,
   materials,
   material,
