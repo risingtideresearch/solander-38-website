12082fbd2fa73fd89257092f317d99a65b9ecea3
Shelby Wilson <s.wilson024@gmail.com>
2026-06-06 14:18:39 -0700

    use minimal drawing manifest for search

--- frontend/app/components/Search/Search.tsx
+++ frontend/app/components/Search/Search.tsx
@@ -2,7 +2,7 @@ import SearchClient from "./SearchClient";
 import { getDrawingsManifest } from "@/app/manifest-util";
 
 export default async function Search({ type = ''}) {
-  const drawings = getDrawingsManifest()
-  
-  return <SearchClient type={type} drawings={drawings.files} />;
+  const { files } = getDrawingsManifest();
+  const drawings = files.map(({ clean_filename, uuid, id }) => ({ clean_filename, uuid, id }));
+  return <SearchClient type={type} drawings={drawings} />;
 }
